7d8a5778dcc688e3070654138525569023665d36
hendisantika <hendisantika@yahoo.co.id>
2024-06-07 09:56:17 +0700

    Add findProductByID in ProductController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/productservice/controller/ProductController.java
+++ src/main/java/id/my/hendisantika/productservice/controller/ProductController.java
@@ -2,12 +2,17 @@ package id.my.hendisantika.productservice.controller;
 
 import id.my.hendisantika.productservice.entity.Product;
 import id.my.hendisantika.productservice.repository.ProductRepository;
+import id.my.hendisantika.productservice.util.ResponseHandle;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -31,4 +36,14 @@ public class ProductController {
         return productRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> findProductByID(@PathVariable Integer id) {
+        Optional<Product> product = productRepository.findById(id);
+
+        if (product.isEmpty()) {
+            return ResponseHandle.generate("Product not found :/", HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(product.get(), HttpStatus.OK);
+    }
 }
