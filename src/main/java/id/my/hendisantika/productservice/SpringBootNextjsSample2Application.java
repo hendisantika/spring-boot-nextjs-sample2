ba945e2f7269a45ffe70dbf11051a3de01e8a693
hendisantika <hendisantika@yahoo.co.id>
2024-06-07 07:21:52 +0700

    Create Spring Boot Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/productservice/SpringBootNextjsSample2Application.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNextjsSample2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootNextjsSample2Application.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/productservice/SpringBootNextjsSample2ApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootNextjsSample2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
