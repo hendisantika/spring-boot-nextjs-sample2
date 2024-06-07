c5088215e0097b3e090121aa277cc9c78c3e8f7f
hendisantika <hendisantika@yahoo.co.id>
2024-06-07 07:29:35 +0700

    Add Client entity 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/productservice/entity/Client.java
@@ -0,0 +1,50 @@
+package id.my.hendisantika.productservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : product-service
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/7/24
+ * Time: 07:27
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Entity
+@Table(name = "clients")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Client implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String cpf;
+    private Double income;
+    private Instant birthDate;
+    private Integer children;
+
+    public Client(String name, String cpf, Double income, Instant birthDate, Integer children) {
+        this.name = name;
+        this.cpf = cpf;
+        this.income = income;
+        this.birthDate = birthDate;
+        this.children = children;
+    }
+}
