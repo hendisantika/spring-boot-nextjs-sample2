bca578a17f347588b0c362b5f99fc1068007b68c
hendisantika <hendisantika@yahoo.co.id>
2024-06-07 07:40:53 +0700

    Add delete in ClientController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/productservice/controller/ClientController.java
+++ src/main/java/id/my/hendisantika/productservice/controller/ClientController.java
@@ -4,6 +4,7 @@ import id.my.hendisantika.productservice.dto.ClientDTO;
 import id.my.hendisantika.productservice.service.ClientServices;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -59,5 +60,11 @@ public class ClientController {
         ClientDTO newClient = clientServices.update(id, dto);
 
         return ResponseEntity.ok().body(newClient);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        clientServices.delete(id);
+        return ResponseEntity.ok().build();
     }
 }
