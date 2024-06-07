d428693fdd9882f903cdebbf61593318e5bb2408
hendisantika <hendisantika@yahoo.co.id>
2024-06-07 07:40:03 +0700

    Add find in ClientController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/productservice/controller/ClientController.java
+++ src/main/java/id/my/hendisantika/productservice/controller/ClientController.java
@@ -5,6 +5,7 @@ import id.my.hendisantika.productservice.service.ClientServices;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,5 +31,12 @@ public class ClientController {
     public ResponseEntity<List<ClientDTO>> findAll() {
         List<ClientDTO> clientList = clientServices.findAll();
         return ResponseEntity.ok().body(clientList);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> find(@PathVariable Long id) {
+        ClientDTO clientFounded = clientServices.findOne(id);
+
+        return ResponseEntity.ok().body(clientFounded);
     }
 }
